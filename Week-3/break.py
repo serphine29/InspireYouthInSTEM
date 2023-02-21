d485d0b63bc338f073b116fe5b5b4dc1db2737f8
serphineachieng52@gmail.com2 <serphineachieng52@gmail.com>
2023-02-21 10:44:28 +0300

    main

---
+++ Week-3/break.py
@@ -0,0 +1,12 @@
+
+
+#break keywork in python
+num = 0
+while (num < 10):
+    print(num)
+    if(num ==5):
+        break
+    print("breaking at...5")
+    num = num + 1
+#write a program that solves quadratic equations
+# using for lop draw a diamond ,triangle, pascals triangle    
---
+++ Week-3/loops.py
@@ -0,0 +1,21 @@
+
+#do while 
+while (True):
+
+   print("my name is serphine")
+
+num = 24
+while(True):
+    if (num > 20):
+        print(num)
+
+
+age = 24
+gender = "male"
+if(age < 18 & gender =("male")):
+    if (weight > 75 | height < 125):
+        print("you are obese")
+    elif
+        print("you are healthy")
+elif
+    print("you are just fine")

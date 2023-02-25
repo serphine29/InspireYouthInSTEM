94c9421ba296914bbf6b8afb43ab86212859f6b8
serphineachieng52@gmail.com2 <serphineachieng52@gmail.com>
2023-02-25 13:09:02 +0300

    main

--- Week-3/factorial.py
+++ Week-3/factorial.py
@@ -7,4 +7,15 @@ def factorial(n):
 
 print( factorial(3))
 
-#write a program to calculate simple interest
+#write a program to calculate simple interest using function
+
+def simple_interest( p ,r ,t):
+    SI = (p * r * t)/100
+    return SI
+p = float(input("Enter the principal amount:"))
+r = float(input("Enter the rate of interest:"))
+t = float(input("Enter the number of years:"))
+
+#calculate simple_interest by using this formula
+SI = simple_interest( p ,r ,t )
+print("simple_interest : {}".format(SI))
